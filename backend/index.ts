00bbf7bf351b51d5c2043d092595998473a12c56
Nirusaki-Malaal <mradulkumar07ocb@gmail.com>
2026-07-15 12:34:56 +0530

    feat: Added Backend Endpoint for IP-Tracking and Simple Functionality

--- backend/index.ts
+++ backend/index.ts
@@ -1,18 +1,18 @@
 import "dotenv/config";
 import express, { Request, Response }from "express"; // importing express
-import authRouter from './routes/auth';
 import rootRouter from './routes/root';
 import cors from "cors";
-import cookieParser from "cookie-parser";
-const app = express(); // creating express object
+
+const app = express(); // Express object
 const PORT: number = 5000;
 
+app.set('trust proxy', true); // Allow Proxies 
 app.use(express.json());
-app.use(cookieParser());
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }))
-app.use('/', rootRouter);
-app.use('/auth', authRouter);
+app.use('/api/getIp', rootRouter); // Routing 
+
 app.listen(PORT , () => console.log(`listening at port ${PORT}`));
---
+++ backend/routes/root.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+    try 
+    {
+    const ip: string = req.ip ?? "UNKNOWN"; // null check
+    const date: Date = new Date();
+    const json = { status: ip };
+    const dateString : String = `${date.getDay()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+
+    console.log(`[IP] ${ip} [TIME] ${dateString}`); // We Have To Enforce IPV4 or IPV6 in the frontend and make two calls
+    
+    return res.status(200).json({ json });
+    }
+    catch (error)
+    {
+        return res.status(400).json({"error" : String(error)});
+    }
+});
+
+export default router;
